2e27c22e501d4ef6d16c5c6f7179b45bd18a70c5
Thiago Costa <thiagodacosta@gmail.com>
2014-10-27 18:18:10 -0200

    Refactor celery module

--- ansible_blog/celery.py
+++ ansible_blog/celery.py
@@ -1,16 +1,12 @@
-# -*- coding:utf-8 -*-
-from __future__ import absolute_import
-
 import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ansible_blog.settings")
 
 from celery import Celery
 
 from django.conf import settings
 
-app = Celery('ansible_blog', broker=settings.BROKER_URL)
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ansible_blog.settings")
 
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
+app = Celery('ansible_blog')
+
 app.config_from_object('django.conf:settings')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
